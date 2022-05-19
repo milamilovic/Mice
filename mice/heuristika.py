de76168d3132113e0d6e0dde3b34abaf42af0a53
milamilovic <milamilovic03@gmail.com>
2022-05-19 23:09:59 +0200

    zavrsene heuristike, treba ih spojiti

--- mice/heuristika.py
+++ mice/heuristika.py
@@ -1,13 +1,11 @@
 import main
 
 def heuristika(tabla, faza, boja):
+    #mozda da postoji ii neka provera taktike
     if faza == 1:
-        pass
-    elif faza == 2:
         pass
     else:
         pass
-        #da li ovo uopšte treba da postoji???
 
 def novi_mlin(tabla, boja):     #vraca brojcanu vrednost koja oznacava razliku nasih i protivickih novih 
     new_mlin = 0                #mlinova je napravljeno u ovom potezu
@@ -64,6 +62,8 @@ def broj_blokiranih_suparnikovih_figura(tabla, boja):           #vraca broj blok
     broj_blokiranih_figura = 0
     for i in range(3):
         for j in range(8):
+            sledece = j + 1
+            prethodno = j - 1
             if j == 0:
                 prethodno = 7
             elif j == 7:
@@ -78,6 +78,8 @@ def broj_blokiranih_suparnikovih_figura(tabla, boja):           #vraca broj blok
                         broj_blokiranih_suparnikovih_figura += 1
     for i in range(3):
         for j in range(8):
+            sledece = j + 1
+            prethodno = j - 1
             if j == 0:
                 prethodno = 7
             elif j == 7:
@@ -107,21 +109,93 @@ def broj_piona(tabla, boja):            #vraca broj piona minus broj piona supar
                 pioni_suparnik += 1
     return pioni - pioni_suparnik
 
-def broj_dvojki(tabla, boja):
-    pass
+def broj_dvojki(tabla, boja):           #vraca broj dvojki koje postoje na tabli za jednu boju
+    pozicija=0
+    broj_dvojki = 0
+    for i in range(len(tabla._izgled)):
+        for j in range(1, len(tabla._izgled[i]), 2):
+            pozicija+=2
+            sledece = j + 1
+            prethodno = j - 1
+            if j == 7:
+                sledece = 0
+            if tabla._izgled[i][j] == boja:
+                if tabla._izgled[i][prethodno] == "x" and tabla._izgled[i][sledece] == boja:
+                    broj_dvojki += 1
+                elif tabla._izgled[i][prethodno] == boja and tabla._izgled[i][sledece] == "x":
+                    broj_dvojki += 1
+                if i in [0, 2]:
+                    if tabla._izgled[1][j] == "x" and tabla._izgled[abs(i - 2)][j] == boja:
+                        broj_dvojki += 1
+                    elif tabla._izgled[1][j] == boja and tabla._izgled[abs(i - 2)][j] == "x":
+                        broj_dvojki += 1
+                elif i==1:
+                    if tabla._izgled[0][j] == "x" and tabla._izgled[2][j] == boja:
+                        broj_dvojki += 1
+                    elif tabla._izgled[0][j] == boja and tabla._izgled[2][j] == "x":
+                        broj_dvojki += 1
+            elif tabla._izgled[i][j] == "x":
+                if tabla._izgled[i][prethodno] == boja and tabla._izgled[i][sledece] == boja:
+                    broj_dvojki += 1
+                if i in [0, 2]:
+                    if tabla._izgled[1][j] == boja and tabla._izgled[abs(i - 2)][j] == boja:
+                        broj_dvojki += 1
+                elif i==1:
+                    if tabla._izgled[0][j] == boja and tabla._izgled[2][j] == boja:
+                        broj_dvojki += 1
+    return broj_dvojki
 
-def broj_trojki(tabla, boja):
-    pass
+def broj_trojki(tabla, boja):           #vraca broj trojki koje postoje na tabli za jednu boju, važi za prvu fazu
+    dva = broj_dvojki(tabla, boja)
+    return ( dva * (dva - 1) ) / 2
 
-def nova_prilika_za_mlin(tabla, boja):
-    pass
-    #k = tabla.broj_dvojki() - tabla._roditelj.broj_dvojki()
-    # if k >=1:
-    #   broj_prilika += k 
+def nova_prilika_za_mlin(tabla, boja):                              #funkcija vraca broj novih prilika za mlin minus za protivnika
+    if boja == "■":
+        boja2 = "▢"
+    else:
+        boja2 = "■"
+    k = tabla.broj_dvojki(tabla, boja) - tabla._roditelj.broj_dvojki(tabla, boja)
+    l = tabla.broj_dvojki(tabla, boja2) - tabla._roditelj.broj_dvojki(tabla, boja2)
+    return k - l
     
 def dupli_mlin(tabla, boja):
-    pass
-    #dva mlina paralelno ili cosak mada to je losije
+    if boja == "■":
+        boja2 = "▢"
+    else:
+        boja2 = "■"
+    broj_duplih = 0
+    for i in range(3):
+        for j in range(0, 8, 2):
+            sledece = j + 1
+            sledece2 = j + 2
+            prethodno = j - 1
+            prethodno2 = j - 2
+            if j == 0:
+                prethodno = 7
+                prethodno2 = 6
+            if tabla[i][j] == "x":
+                if tabla[i][prethodno]==tabla[i][prethodno2]==tabla[i][sledece]==tabla[i][sledece2]==boja:
+                    broj_duplih += 1
+                elif tabla[i][prethodno]==tabla[i][prethodno2]==tabla[i][sledece]==tabla[i][sledece2]==boja2:
+                    broj_duplih -= 1
+    for j in range(1, 8, 2):
+        if j==7:
+            sledece=0
+        else:
+            sledece = j + 1
+        if tabla[1][j]=="x" and tabla[0][j]==tabla[2][j]==tabla[1][sledece]==tabla[1][j-1]==boja:
+            broj_duplih += 1
+        elif tabla[1][j]=="x" and tabla[0][j]==tabla[2][j]==tabla[1][sledece]==tabla[1][j-1]==boja2:
+            broj_duplih -= 1
+        elif tabla[0][j] == "x" and tabla[0][j-1]==tabla[0][sledece]==tabla[1][j]==tabla[2][j]==boja:
+            broj_duplih += 1
+        elif tabla[0][j] == "x" and tabla[0][j-1]==tabla[0][sledece]==tabla[1][j]==tabla[2][j]==boja2:
+            broj_duplih -= 1
+        elif tabla[2][j] == "x" and tabla[2][j-1]==tabla[2][sledece]==tabla[1][j]==tabla[0][j]==boja:
+            broj_duplih += 1
+        elif tabla[2][j] == "x" and tabla[2][j-1]==tabla[2][sledece]==tabla[1][j]==tabla[0][j]==boja2:
+            broj_duplih -= 1
+            
 
 def pobednicka_konfiguracija(tabla, boja):      #vraca 1000 za pobednicku konfig, a nula ako nije
     if boja == "■":
@@ -132,5 +206,3 @@ def pobednicka_konfiguracija(tabla, boja):      #vraca 1000 za pobednicku konfig
         return 1000
     else:
         return 0
-
-
